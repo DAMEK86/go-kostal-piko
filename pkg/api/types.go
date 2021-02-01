9f94a48a20b2e27e44e40da939dbb25d8b6575c9
Andreas Rehn <rehn.andreas86@gmail.com>
2021-02-01 23:09:49 +0100

    api: add operation status values

--- pkg/api/types.go
+++ pkg/api/types.go
@@ -27,3 +27,27 @@ type DxsRespone struct {
 	Session DxsSession `json:"session"`
 	Status  DxsStatus  `json:"status"`
 }
+
+// TODO: provide method for status mapping
+type OperatingStatus string
+
+const (
+	// value 0
+	Off OperatingStatus = "off"
+	// value 1
+	Standby OperatingStatus = "standby"
+	// value 2
+	Starting OperatingStatus = "starting"
+	// value 3
+	FeedInMPP OperatingStatus = "Feed in (MPP)"
+	// value 4
+	FeedInLimited OperatingStatus = "Feed in limited"
+	// value 5
+	FeedIn OperatingStatus = "Feed In"
+	// value 6
+	DcPowerLow OperatingStatus = "DC voltage low"
+	// value 7
+	InsulationMeasurement OperatingStatus = "Insulation measurement"
+	// value 8
+	WaitingTimeActive OperatingStatus = "Waiting time active!"
+)
